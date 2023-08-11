5be799a6f6188d15e650b3d4c33be1dd43a56f22
Osama-Abo-Reda <osamaaboureida@gmail.com>
2023-08-11 14:43:56 -0700

    Edit 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
-int main(){
+/**
+ * Description : main -> entry point
+ *
+ * It's a C programming print with puts function
+ *
+ * Return :ALawys 0 (Success)
+*/
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
