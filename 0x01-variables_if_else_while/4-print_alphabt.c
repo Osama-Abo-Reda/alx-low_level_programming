06c64c35c1e99fa3e9f8c09f5cb1fe7187454ba8
Osama-Abo-Reda <osamaaboureida@gmail.com>
2023-08-11 17:46:47 -0700

    Create  4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Description: prints the alphabet in lowercase except q and e
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			ch++;
+			continue;
+		}
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
