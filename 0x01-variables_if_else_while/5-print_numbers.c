0385992e9b8de666ee6996e2b2fca036c716a668
Osama-Abo-Reda <osamaaboureida@gmail.com>
2023-08-11 18:00:01 -0700

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0(Success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf('\n');
+
+	return (0);
+}
