1c22d6ecf8c29571f24e34220bf3767e224a8a2a
Osama-Abo-Reda <osamaaboureida@gmail.com>
2023-08-15 18:59:03 -0700

    Edit 1-alphabet.c file of betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
+ *  print_alphabet - function print alphabet by using _putchar function
  *
- * decsription: print_alphabet function
- *
- * Return: Always 0.
 */
 
 void print_alphabet(void)
