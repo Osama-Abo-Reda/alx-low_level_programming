0f66848ec0b5255092da1913e30b90b75766def7
Osama-Abo-Reda <osamaaboureida@gmail.com>
2023-08-15 19:14:04 -0700

    Create 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that will print the alphabet 10 time,
+ * followed by a new line
+ *
+*/
+
+void print_alphabet_x10(void);
+{
+	char num;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (num = 'a'; num <= 'z; num++)
+			_putchar(num);
+		_putchat('\n');
+		i++;
+	}
+}
